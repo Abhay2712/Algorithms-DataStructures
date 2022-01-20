3c78fc9a137a448f017507233a5ca01649108075
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-21 01:10:22 +0530

    Time: 4 ms (70.76%), Space: 7.5 MB (98.87%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,27 +3,20 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>res;
         vector<int>ds;
-        int n=nums.size();
-        int freq[n];
-        for(int i=0;i<n;i++) freq[i]=0;
-        subfun(res,nums,ds,freq);
+        subfun(res,nums,0);
         return res;
     }
     
-    void subfun(vector<vector<int>>&res,vector<int>&nums,vector<int>&ds,int freq[]){
-        if(ds.size()==nums.size()){
-            res.push_back(ds);
+    void subfun(vector<vector<int>>&res,vector<int>&nums,int ind){
+        if(ind==nums.size()){
+            res.push_back(nums);
+            return;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                subfun(res,nums,ds,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
-        }
-        
+        for(int i=ind;i<nums.size();i++){
+                swap(nums[ind],nums[i]);
+                subfun(res,nums,ind+1);
+                swap(nums[ind],nums[i]);
+        }        
     }
 };
