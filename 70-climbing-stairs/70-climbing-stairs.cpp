c06c3a188c77eab2ea345e08cc77ff400cf5d7d5
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-24 03:02:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.32%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-          int f1=1,f2=2,ans=0;
+      int f1=1,f2=2,ans;
       for(int i=1;i<=n;i++)
       {
-        
-        // if(i==0)
-        //   ans= 0;else 
         if(i==1)
-          ans= 1;
+          ans=1;
         else if(i==2)
-          ans= 2;
+          ans=2;
         else
           ans=f1+f2;
           f1=f2;
           f2=ans;
       }
-      return ans;
-        
+      return ans;      
     }
-    
 };
