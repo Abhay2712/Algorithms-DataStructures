63c493a976af67468ffda334b9ca12554eda0b4d
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-25 00:09:46 +0530

    Added solution - LeetHub

--- Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.cpp
+++ Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.cpp
@@ -10,15 +10,15 @@ public:
         int count=0,sum=0;
         for(int i=0;i<S.size();i++){
             if(S[i]==']'){
-                count++;
+                count++;    //keep adding until you find opening bracket
             }
             else {
                 if(count>0){
-                sum+=count;
-                }
-                count--;
-            }
-        }
+                sum+=count; //as we find the opening bracket we will do the count number
+                }           // of shifts to make it reach the first closing bracket(2 swaps)
+                count--;    //]][[  ][][    []][
+            }               //now we found the 2nd opening bracket and count=1 so we did 1 swap
+        }                   //[][]
         return sum;
     }
 };
