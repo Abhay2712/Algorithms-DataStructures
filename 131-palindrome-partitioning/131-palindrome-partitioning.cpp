0460df503fee0d4a5204f734c5dede2af5d44290
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-25 01:00:45 +0530

    Time: 196 ms (47.51%), Space: 57.6 MB (56.91%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>res;
+        vector<string>ds;
+        subfun(res,ds,s,0);
+        return res;
+        
+    }
+    
+    void subfun(vector<vector<string>>&res,vector<string>&ds,string s,int ind){
+        
+        if(ind==s.size()){
+            res.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<s.size();++i){
+            if(isPal(s,ind,i)){
+                ds.push_back(s.substr(ind,i-ind+1));
+                subfun(res,ds,s,i+1);
+                ds.pop_back();
+            }
+        }
+    }
+    
+    bool isPal(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
