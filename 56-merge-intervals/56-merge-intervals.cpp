d008ff1eb7732155f8c2f870d044b5b5042aad8d
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-25 23:47:00 +0530

    Time: 62 ms (23.27%), Space: 18.9 MB (37.54%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        if(arr.size() == 1){return arr;}
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        ans.push_back(arr[0]);
+        int p1 = 0;
+        int p2 = 1;
+        while(p2<arr.size()){
+            if(ans[p1][0]<=arr[p2][0] && ans[p1][1]>= arr[p2][1]){
+                p2++;
+            }
+            else if(ans[p1][1] >= arr[p2][0]){
+                ans[p1][1] = arr[p2][1];
+                p2++;
+            }
+            else{
+                ans.push_back(arr[p2]);
+                p1++; p2++;
+            }
+        }
+        return ans;
+    }
+};
