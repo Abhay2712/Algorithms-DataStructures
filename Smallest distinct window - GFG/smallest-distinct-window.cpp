61bccda4c42581288c53cbf83b5e08aac0a707aa
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-25 23:01:56 +0530

    Added solution - LeetHub

---
+++ Smallest distinct window - GFG/smallest-distinct-window.cpp
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    const int MAX_CHARS = 256;
+    string findSubString(string str)
+    {
+    int n = str.length();
+ 
+// if string is empty or having one char
+    if (n <= 1)
+        return str;
+ 
+// Count all distinct characters.
+    int dist_count = 0;
+    bool visited[MAX_CHARS] = { false };
+    for (int i = 0; i < n; i++) {
+        if (visited[str[i]] == false) {
+            visited[str[i]] = true;
+            dist_count++;
+        }
+    }
+    
+    int start = 0, start_index = -1, min_len = INT_MAX;
+ 
+    int count = 0;
+    int curr_count[MAX_CHARS] = { 0 };
+    for (int j = 0; j < n; j++) {
+        // Count of characters of string in the window
+        curr_count[str[j]]++;
+ 
+        // If any distinct character matched,
+        // then increment count
+        if (curr_count[str[j]] == 1)
+            count++;
+ 
+        // if all the characters are matched
+        if (count == dist_count) {
+            // Try to minimize the window i.e., check if
+            // any character is occurring more no. of times
+            // than its occurrence in pattern, if yes
+            // then remove it from starting and also remove
+            // the useless characters.
+            while (curr_count[str[start]] > 1) {
+                if (curr_count[str[start]] > 1)
+                    curr_count[str[start]]--;
+                start++;
+            }
+            
+            int len_window = j - start + 1;
+            if (min_len > len_window) {
+                min_len = len_window;
+                start_index = start;
+            }
+        }
+    }
+ 
+    // Return substring starting from start_index
+    // and length min_len
+    return str.substr(start_index, min_len);
+}
+};
+
+// { Driver Code Starts.
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.findSubString(str).size() << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
