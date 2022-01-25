e7fb049f384ab497476a8c0a4cfd3b82e8104d75
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-01-25 23:59:16 +0530

    Time: 90 ms (7.68%), Space: 19.1 MB (30.35%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        if(arr.size() == 1){return arr;}
+        if(arr.size() == 1) return arr;
         sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
-        ans.push_back(arr[0]);
-        int p1 = 0;
-        int p2 = 1;
+        vector<vector<int>> ans={arr[0]};
+        int p1 = 0,p2=1;
         while(p2<arr.size()){
             if(ans[p1][1]>= arr[p2][1]){
                 p2++;
