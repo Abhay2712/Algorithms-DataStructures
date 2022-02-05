b1033e40c2d38623bad54b9f73799859153fa145
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-02-06 01:58:44 +0530

    Time: 22 ms (32.60%), Space: 20.9 MB (52.33%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -12,33 +12,18 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return dfsHeight (root) != -1;
+        return height(root)!=-1;
     }
-    int dfsHeight (TreeNode *root) {
-        if (root == NULL) return 0;
+    
+    int height(TreeNode *root){
+        if(!root) return 0;
         
-        int leftHeight = dfsHeight (root -> left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = dfsHeight (root -> right);
-        if (rightHeight == -1) return -1;
+        int lh=height(root->left);
+        if(lh==-1) return -1;
+        int rh=height(root->right);
+        if(rh==-1) return -1;
         
-        if (abs(leftHeight - rightHeight) > 1)  return -1;
-        return max (leftHeight, rightHeight) + 1;
+        if((abs(lh-rh)>1)) return -1;
+        return max(lh,rh) + 1;
     }
 };
-//     bool isBalanced(TreeNode* root) {
-//         return height(root)!=-1;
-//     }
-    
-//     int height(TreeNode *root){
-//         if(!root) return 0;
-        
-//         int lh=height(root->left);
-//         if(lh==-1) return -1;
-//         int rh=height(root->right);
-//         if(rh==-1) return -1;
-        
-//         if(abs(lh-rh>1)) return -1;
-//         return max(lh,rh) + 1;
-//     }
-// };
