d9b895e0db8ca1e3c4e5e1423f6bf15735fd0082
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-02-21 23:55:46 +0530

    Time: 42 ms (8.25%), Space: 6 MB (32.14%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int x) {
         string s=to_string(x);
         string t=s;
-        reverse(t.begin(),t.end());
+        reverse(t.begin(),t.end()); //normal solution
         if(s==t) return 1;
         return 0;
     }
