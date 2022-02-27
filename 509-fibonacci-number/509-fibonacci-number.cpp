f2df7b375e0bd073a48247ab21392657c4fc42ff
Abhay2712 <68679022+Abhay2712@users.noreply.github.com>
2022-02-27 23:07:39 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        int arr[n+1];
-        memset(arr,-1,sizeof(arr));
+        vector<int>arr(n+1,-1);
+        // memset(arr,-1,sizeof(arr));
         return subfun(n,arr);
     }
     
-    int subfun(int n,int arr[]){        
+    int subfun(int n,vector<int>&arr){        
         if(n<=1) return n;
         if(arr[n]!=-1) return arr[n];
-        return arr[n]=(fib(n-1)+fib(n-2));       
+        return arr[n]=(subfun(n-1,arr)+subfun(n-2,arr));       
     }
 };
