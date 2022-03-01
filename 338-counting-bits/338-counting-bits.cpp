8f88e196a2cd3f66b32d299854ffb6901ac2bddd
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-01 11:33:27 +0530

    Time: 6 ms (65.69%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>res(n+1,0);
-        int i=0;
-        while(i<=n){
-            res[i]=__builtin_popcount(i);
-            i++;
-        }
-        return res;
+        vector<int> t(n+1);
+        t[0] = 0; // t[0] will be 0 beacuse 0 has count of set bit is 0;
+        
+        // we can compute current set bit count using previous count as x/2 in O(1) time
+        // i%2 will be 0 for even number ans 1 for odd number
+        
+        for(int i = 1; i<=n; ++i)
+            t[i] = t[i/2] + i%2;
+        
+        return t;
     }
 };
