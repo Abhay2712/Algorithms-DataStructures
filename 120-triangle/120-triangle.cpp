b119a4aedb692f2878ed095caff1d31bb4e7efcd
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-04 00:04:52 +0530

    Time: 14 ms (20.84%), Space: 9 MB (22.06%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -7,7 +7,7 @@ public:
     }
     
     int subfun(int i,int j,vector<vector<int>>&triangle,vector<vector<int>>&dp,int n){
-        if(i==n-1) return triangle[i][j];
+        if(i==n-1) return triangle[i][j];   //here we going from 0 to n
         
         if(dp[i][j]!=-1) return dp[i][j];
         int down=subfun(i+1,j,triangle,dp,n);
@@ -15,3 +15,25 @@ public:
         return dp[i][j]=triangle[i][j]+min(down,diag);
     }
 };
+
+//tabulation
+// int minimumPathSum(vector<vector<int> > &triangle, int n){
+//     vector<vector<int> > dp(n,vector<int>(n,0));
+    
+//     for(int j=0;j<n;j++){
+//         dp[n-1][j] = triangle[n-1][j];
+//     }
+    
+//     for(int i=n-2; i>=0; i--){
+//         for(int j=i; j>=0; j--){
+            
+//             int down = triangle[i][j]+dp[i+1][j];
+//             int diagonal = triangle[i][j]+dp[i+1][j+1];
+            
+//             dp[i][j] = min(down, diagonal);
+//         }
+//     }
+    
+//     return dp[0][0];
+    
+// }
