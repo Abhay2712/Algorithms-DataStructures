838a2898cff4612fa50d224edbc95e6d1bd67a97
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-04 00:37:07 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -34,6 +34,36 @@ public:
         return dp[i][j]=matrix[i][j]+max(up,max(ld,rd));
     }
 };
+//tabulation approach
+// int getMaxPathSum(vector<vector<int> > &matrix){
+    
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+    
+//     vector<vector<int>> dp(n,vector<int>(m,0));
+    
+//     // Initializing first row - base condition
+//     for(int j=0; j<m; j++){
+//         dp[0][j] = matrix[0][j];
+//     }
+    
+//     for(int i=1; i<n; i++){
+//         for(int j=0;j<m;j++){
+            
+//             int up = matrix[i][j] + dp[i-1][j];
+            
+//             int leftDiagonal= matrix[i][j];
+//             if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+//             else leftDiagonal += -1e9;
+            
+//             int rightDiagonal = matrix[i][j];
+//             if(j+1<m) rightDiagonal += dp[i-1][j+1];
+//             else rightDiagonal += -1e9;
+            
+//             dp[i][j] = max(up, max(leftDiagonal,rightDiagonal));
+            
+//         }
+//     }
 
 // { Driver Code Starts.
 
