13aafe86d6f1ef8f4627d613a032776c58c569ee
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-03 10:42:10 +0530

    Time: 0 ms (100.00%), Space: 8 MB (11.48%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -20,3 +20,43 @@ public:
       return dp[i][j] = up+left;
     }
 };
+
+//tabulation method
+// int mazeObstaclesUtil(int n, int m, vector<vector<int>> &maze,vector<vector<int>>
+//  &dp) 
+// {
+//   for(int i=0; i<n ;i++){
+//       for(int j=0; j<m; j++){
+          
+//           //base conditions
+//           if(i>0 && j>0 && maze[i][j]==-1){
+//             dp[i][j]=0;
+//             continue;
+//           }
+//           if(i==0 && j==0){
+//               dp[i][j]=1;
+//               continue;
+//           }
+          
+//           int up=0;
+//           int left = 0;
+          
+//           if(i>0) 
+//             up = dp[i-1][j];
+//           if(j>0)
+//             left = dp[i][j-1];
+            
+//           dp[i][j] = up+left;
+//       }
+//   }
+  
+//   return dp[n-1][m-1];
+
+  
+// }
+
+// int mazeObstacles(int n, int m, vector<vector<int> > &maze){
+//     vector<vector<int> > dp(n,vector<int>(m,-1));
+//     return mazeObstaclesUtil(n,m,maze,dp);
+    
+// }
