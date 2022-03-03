8fab2e80762460e6349280f4840909e20dffa574
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-03 14:13:23 +0530

    Time: 32 ms (5.34%), Space: 10 MB (52.16%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -18,3 +18,28 @@ public:
         
     }
 };
+
+//tabular method
+// int minSumPath(int n, int m, vector<vector<int> > &matrix){
+//     vector<vector<int> > dp(n,vector<int>(m,0));
+//     for(int i=0; i<n ; i++){
+//         for(int j=0; j<m; j++){
+//             if(i==0 && j==0) dp[i][j] = matrix[i][j];
+//             else{
+                
+//                 int up = matrix[i][j];
+//                 if(i>0) up += dp[i-1][j];
+//                 else up += 1e9;
+                
+//                 int left = matrix[i][j];
+//                 if(j>0) left+=dp[i][j-1];
+//                 else left += 1e9;
+                
+//                 dp[i][j] = min(up,left);
+//             }
+//         }
+//     }
+    
+//     return dp[n-1][m-1];
+    
+// }
