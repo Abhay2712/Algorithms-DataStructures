35b762a76866a107652b77a25ca00b49b09fb265
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-04 23:23:01 +0530

    Time: 152 ms (5.57%), Space: 43.1 MB (9.62%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -3,10 +3,11 @@ public:
     double champagneTower(int poured, int query_row, int query_glass) {
         vector<vector<double>>dp(101,vector<double>(101));
         dp[0][0]=poured;
-        for (int r = 0; r <= query_row; ++r) {
-            for (int c = 0; c <= r; ++c) {
+        for (int r = 0; r<query_row; ++r) {
+            for (int c = 0; c<=r; ++c) {
                 double res = (dp[r][c] - 1.0) / 2.0;
                 if (res > 0) {
+                    cout<<dp[r][c];
                     dp[r+1][c] += res;
                     dp[r+1][c+1] += res;
                 }
