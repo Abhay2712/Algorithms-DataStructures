e0ae6d39e3c36434242b642423ab145910c32e3a
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-05 23:29:55 +0530

    Time: 8 ms (77.52%), Space: 14 MB (10.99%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-		int n = 10001;    
-        if (nums.size() == 1) return nums[0];
-
-        vector<int> map(n, 0);
-        
-        for (auto &num: nums) {
-            map[num] += num;
-        }
-        int a = map[0];
-        int b = max(a,map[1]);
-        
-        for (int i = 2; i < n; i++) {
-            int cur = max(b,a+map[i]);
-            a=b;
-            b=cur;
-            
-        }
-        return max(a,b);     
+    int n = 10001;
+    
+	//take the total sum by each number
+    vector<int> sum(n, 0);
+    vector<int> dp(n, 0);
+    
+    for(auto num: nums){
+        sum[num] += num;
     }
+    
+    dp[0] = 0;
+    dp[1] = sum[1];
+    //now apply the house robbing concept
+    for(int i=2; i<n; i++){
+        dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+    }
+    
+    return dp[n-1];
+}
 };
