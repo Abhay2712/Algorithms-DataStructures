099453fed10924dbdcd5110fc17daa2e826bfb0f
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-03-15 00:29:29 +0530

    Time: 151 ms (73.17%), Space: 49.9 MB (34.46%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -4,16 +4,15 @@ public:
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int>pnc(nums.size(),1);
-        for(int i=1;i<nums.size();i++){
-            pnc[i]=2*pnc[i-1]%mod;
+        for(int i=1;i<nums.size();i++){         //there will be 2 options for each- take or not take
+            pnc[i]=2*pnc[i-1]%mod;              //so using pnc and dp find all the possible outputs and save in an array
         }
         int i=0,j=nums.size()-1,count=0;
         
         while(i<=j){
             if(nums[i]+nums[j]<=target) {
-                // cout<<nums[i]+nums[j]<<" ";
-                count=(count+pnc[j-i])%mod;
-                i++;
+                count=(count+pnc[j-i])%mod;     //using 2 pointers if found add the permutations from array
+                i++;                            //else just reduce the position of maximum element
             }
             else j--;
         }
