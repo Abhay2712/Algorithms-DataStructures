f17399fb628d4a28bf7294d54ed1fdadfc3ac416
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-04-08 19:25:06 +0530

    Time: 41 ms (64.44%), Space: 19.9 MB (63.09%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -4,8 +4,8 @@ public:
     int size;
     KthLargest(int k, vector<int> nums) {
         size=k;
-        for(int i=0;i<nums.size();i++) {
-            pq.push(nums[i]);
+        for(auto i:nums) {
+            pq.push(i);
             if(pq.size()>k) pq.pop();
         }
     }
