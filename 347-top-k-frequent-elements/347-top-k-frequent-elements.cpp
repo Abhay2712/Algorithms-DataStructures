797b08d88984a5ca9181e439e9701ce54c671d6b
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-04-09 19:24:47 +0530

    Time: 15 ms (86.09%), Space: 13.7 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,51 @@
+//using map and priority queue
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int>maps;
-        map<int,vector<int>>maps1;
+        unordered_map<int, int> N;
+        priority_queue<pair<int, int>> Q;
         for(int i=0;i<nums.size();i++){
-            maps[nums[i]]++;
+            N[nums[i]]++;
         }
         
-        for(auto i:maps){
-            maps1[-i.second].push_back(i.first);
+        for(auto it=N.begin();it!=N.end();it++)
+        {
+            Q.push({it->second,it->first});
         }
         
-        vector<int>ans;
-        for(auto i:maps1){
-            for(auto j:i.second){
-                if(k--) ans.push_back(j);    
-                else break;
-            }
-            if(!k) break;
+        vector<int> R;
+        
+       while(k--){
+            R.push_back(Q.top().second);
+            Q.pop();
         }
-        return ans;
+        
+        return R;
     }
 };
+
+// using 2 maps
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         map<int,int>maps;
+//         map<int,vector<int>>maps1;
+//         for(int i=0;i<nums.size();i++){
+//             maps[nums[i]]++;
+//         }
+        
+//         for(auto i:maps){
+//             maps1[-i.second].push_back(i.first);
+//         }
+        
+//         vector<int>ans;
+//         for(auto i:maps1){
+//             for(auto j:i.second){
+//                 if(k--) ans.push_back(j);    
+//                 else break;
+//             }
+//             if(!k) break;
+//         }
+//         return ans;
+//     }
+// };
