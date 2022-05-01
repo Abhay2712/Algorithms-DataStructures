caebc1451b192e64a29c1f580e68c3e11d554cc4
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-01 10:42:03 +0530

    Time: 4 ms (28.12%), Space: 6 MB (99.98%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        string ans1="",ans2="";
+        int s1=0,s2=0,i=n-1,j=m-1;
+        while(i>=0 || j>=0){
+            if(i>=0){
+            if(s[i]=='#') {s1++;}
+            else{
+                while(i>=0 && s1!=0){
+                    if(s[i]=='#') s1++;
+                    else s1--;
+                    i--;
+                    
+                }
+                if(i>=0)
+                    if(s[i]=='#') s1++;
+                    else
+                        ans1+=s[i];
+            }}
+            if(j>=0){
+            if(t[j]=='#') {s2++;}
+            else{
+                while(j>=0 && s2!=0){
+                    if(t[j]=='#') s2++;
+                    else s2--;
+                    j--;
+                    
+                }
+                if(j>=0)
+                    if(t[j]=='#') s2++;
+                    else ans2+=t[j];
+            }}
+            i--;
+            j--;
+        }
+        // while(j>=0){
+        //     ans2+=t[j--];
+        // }
+        // while(i>=0){
+        //     ans1+=s[i--];
+        // }
+        cout<<ans1<<"1"<<ans2<<"2"<<'\n';
+        return ans1==ans2;
+    }
+};
