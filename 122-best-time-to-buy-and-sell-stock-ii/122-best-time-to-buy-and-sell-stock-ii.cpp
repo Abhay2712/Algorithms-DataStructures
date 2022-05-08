8cd9531e9915e1383e2e5f5e4be5687bdab9103b
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-08 12:52:19 +0530

    Time: 22 ms (5.83%), Space: 13.2 MB (18.62%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,8 +3,7 @@ public:
     int maxProfit(vector<int>& arr) {
         int n=arr.size();
         int min=arr[0];
-        int prof=0;
-        int max=0;
+        int prof=0,max=0;
         for(int i=1;i<n;i++){
             if(arr[i]>min){
                 prof+=arr[i]-min;  
