464ac2465d32efa3164bde95dc40b5975069f558
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-21 13:39:17 +0530

    Time: 22 ms (82.78%), Space: 14.6 MB (28.28%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool dfs(vector<int> adj[],vector<bool> &vis,int start,vector<bool> &ancestor)
-       {    vis[start]=true;
-            ancestor[start]=true;
-            for(auto node: adj[start])
-              {      if(!vis[node])
-                       {      if(dfs(adj,vis,node,ancestor))
-                                   return true;             
-                       }
-                    else if(ancestor[node])
-                      return true;
-              }
-         ancestor[start]=false;
-         return false;
-     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        bool ans=true;
-        vector<int> adj[numCourses];
-        vector<bool> vis(numCourses,false);
-        vector<bool> ancestor(numCourses,false);
-        
-         for(int i=0;i<prerequisites.size();i++)
-         {  
-             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-          }
-      for(int i=0;i<numCourses;i++)
-         {    if(!vis[i])
-                  { if(dfs(adj,vis,i,ancestor))
-                       return false;  
-                  }
+    bool cdfs(int i,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&extra){
+        visited[i]=1;
+        extra[i]=1;
+        for(auto it:adj[i]){
+            if(!visited[it])
+            {
+                if(cdfs(it,adj,visited,extra)) return 1;
+            }
+            else if(extra[it]) return 1;
         }
-        return true;      
+        extra[i]=0;
+        return 0;
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(n,vector<int>());
+        vector<bool>visited(n,0);
+        vector<bool>extra(n,0);
+        for(auto i:pre){
+            adj[i[0]].push_back(i[1]);
+        }
+        
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(cdfs(i,adj,visited,extra)) return 0;        
+            }
+        }
+        return 1;
     }
 };
