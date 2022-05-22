4f243739602c9cc589628b9659f7cd74b28c7be3
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-22 20:30:29 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.85%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto i:s){
+            if(i=='(' || i=='{' || i=='['){
+                st.push(i);
+            }
+            
+            else if(st.empty() || (i==')' && st.top()!='(')||(i==']' && st.top()!='[')||(i=='}' && st.top()!='{')) return false;
+            else st.pop();
+        }
+        return st.empty();        
+    }
+};
