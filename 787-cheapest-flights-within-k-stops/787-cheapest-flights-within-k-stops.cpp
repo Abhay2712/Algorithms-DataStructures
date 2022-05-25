1dab61e338768de4b38979265224384d12c93691
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-25 12:46:24 +0530

    Time: 31 ms (79.49%), Space: 13.6 MB (53.07%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int dp[101][101];
 	
-    int dfs(vector<vector<pair<int, int>>>& graph, int src, int dst, int k) {
+    int dfs(vector<vector<pair<int, int>>>& adj, int src, int dst, int k) {
         if (src == dst) return 0;
         if (k <= -1) return INT_MAX;
         
         if (dp[src][k] != -1) return dp[src][k];
         
         int ans = INT_MAX;
-        for (auto child : graph[src]) {
-            int res = dfs(graph, child.first, dst, k-1);
+        for (auto child : adj[src]) {
+            int res = dfs(adj, child.first, dst, k-1);
             if (res != INT_MAX)
                 ans = min(ans, child.second +res);
         }
@@ -20,12 +20,12 @@ public:
     
       int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
         memset(dp, -1, sizeof dp);
-        vector<vector<pair<int, int>>> graph(n);
-        for (auto vec : flights) {
-            graph[vec[0]].push_back(make_pair(vec[1], vec[2]));
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto i : flights) {
+            adj[i[0]].push_back(make_pair(i[1], i[2]));
         }
           
-        int ans = dfs(graph, src, dst, K);  // or make the dfs call
+        int ans = dfs(adj, src, dst, K);  
         if (ans == INT_MAX)
             return -1;
         return ans;
