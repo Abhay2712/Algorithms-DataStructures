21397597a5fae5d9b3af25489e409eef33ea003f
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-27 16:22:32 +0530

    Time: 797 ms (12.76%), Space: 39.4 MB (11.75%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>res;
         sort(nums.begin(),nums.end());
         if(nums.size()<3) return {};
         if(nums[0] > 0) return {};
         
         unordered_map<int,int>map;
         vector<int>temp;
-        set<vector<int>>s;
         for(int i=0;i<nums.size();i++){
             map[-nums[i]]=i;
         }
@@ -17,11 +17,12 @@ public:
             for(int j=i+1;j<n-1;j++){
                 if(map[nums[i]+nums[j]] && map[nums[i]+nums[j]]>j){
                     temp={nums[i],nums[j],-nums[i]-nums[j]};
-                    s.insert(temp);
+                    res.push_back(temp);
                 }
+                j=map[-nums[j]];
             }
+            i=map[-nums[i]];
         }
-        vector<vector<int>>res(s.begin(),s.end());
         return res;
     }
 };
