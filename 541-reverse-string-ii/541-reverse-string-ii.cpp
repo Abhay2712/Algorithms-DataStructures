66ec7d41aac67dc3e5976b229f209d9ebc3b7df3
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-28 14:26:47 +0530

    Time: 2 ms (90.43%), Space: 7.4 MB (32.57%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int i=1,start,end,last=0,n=s.size()-1; 
-        while(k*(i)-1<s.size()){
+        
+        while(k*(i)-1<s.size()){    //loop to reverse current k elements
             start=k*(i-1);
             end=k*i-1;
             while(start<end)
                 swap(s[start++],s[end--]);
-            i+=2;
-            last=k*(i-1);
+            i+=2;                   //added 2 to skip next k elements
+            last=k*(i-1);           //in case next k elements are not available to reverse store the last element index 
         }
         
-        while(last<n){
+        while(last<n){  //reverse the remaining elements from last to end of string
             swap(s[last++],s[n--]);
         }
         return s;
