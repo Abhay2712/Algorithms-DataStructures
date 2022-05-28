153f4d087f38c075996cd3fca86f48f47307f34e
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-28 18:07:26 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (55.40%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -2,18 +2,18 @@ class MyStack {
 public:
     /** Initialize your data structure here. */
     queue<int> q1;
-    queue<int> q2;
     MyStack() {
         
     }
     
     /** Push element x onto stack. */
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());    q1.pop();
+        int sz = q1.size();
+        q1.push(x);
+        while(sz--){
+            q1.push(q1.front());
+            q1.pop();
         }
-        swap(q1, q2);
     }
     
     /** Removes the element on top of the stack and returns that element. */
