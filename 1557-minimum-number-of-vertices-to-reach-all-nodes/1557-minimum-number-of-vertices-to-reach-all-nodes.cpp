31baff14602693ec89b7eec4409fcdb42bea1dde
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-29 12:35:19 +0530

    Time: 617 ms (10.17%), Space: 111.4 MB (16.83%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(n);
+        
+        vector<int>indegree(n,0);  
+        for(auto i:edges){
+            indegree[i[1]]=1; 
+        }
+        
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            if( indegree[i]==0){
+                res.push_back(i); // add the one that is not reachable by other in res 
+            }
+        }
+        return res;
+    }
+};
