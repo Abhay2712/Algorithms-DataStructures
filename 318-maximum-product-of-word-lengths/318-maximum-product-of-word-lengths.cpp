8aec280df85ab2bb8d784df32ed24593575a0e97
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-29 12:13:14 +0530

    Time: 194 ms (28.28%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    static bool compare(string &s1,string &s2)
+    {
+        return s1.size()>s2.size();
+    }
+    
     int maxProduct(vector<string>& words) {
 	int n = size(words), ans = 0;
 	vector<bitset<26> > chars(n);
 	for(int i = 0; i < n; i++) {
-		for(auto& ch : words[i])  // map the letters to 1 if it occurs in the word
+		for(auto ch : words[i])  // map the letters to 1 if it occurs in the word
 			chars[i][ch - 'a'] = 1;
-		// now check with all other words and if there's no common letter, then update ans	
+
 		for(int j = 0; j < i; j++)
 			if(!checkCommon(chars[i], chars[j])) 
 				ans = max(ans, int(size(words[i]) * size(words[j])));
