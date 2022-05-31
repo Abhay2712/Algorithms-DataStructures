7f78064c3cc840916dc7e11cda20c501b25e12b6
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-31 12:10:10 +0530

    Time: 25 ms (16.42%), Space: 6 MB (78.88%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    const string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
     string intToRoman(int N) {
         string ans = "";
-        for (int i = 0; N; i++)
-            while (N >= val[i]) ans += roman[i], N -= val[i];
+        for (int i = 0; N; i++){
+            while (N >= val[i]) {
+                ans += roman[i];
+                N -= val[i];
+            }
+        }
         return ans;
     }
 };
