a13ebbc7b74f319f2bf7a29d8c919ea6d853dd9a
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-05-31 15:15:07 +0530

    Time: 58 ms (12.52%), Space: 26.1 MB (34.95%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    vector<int> decode(vector<int>& encoded, int first) 
-    {
-        int n = encoded.size() + 1;
-        vector <int> decoded(n);
-        decoded[0] = first;
-        for(int i = 1; i < n; i++)
-            decoded[i] = encoded[i-1]^decoded[i-1];
-        return decoded;
+    vector<int> decode(vector<int>& A, int cur) {
+        vector<int> res = {cur};
+        for (int& a: A)
+            res.push_back(cur ^= a);
+        return res;
     }
 };
