36967273e2eec08e679cbfed0f9ed0f5322ca96e
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-04 21:41:47 +0530

    Time: 9 ms (44.23%), Space: 10.3 MB (13.15%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -5,13 +5,14 @@ public:
         priority_queue<int,vector<int>,greater<int>>q2;
         for(auto i:nums){
             q1.push(i);
-            q2.push(i);
+            // q2.push(i);
         }
         int a1=q1.top()-1; q1.pop();
         int b1=q1.top()-1; q1.pop();
-        int a2=q2.top()-1; q2.pop();
-        int b2=q2.top()-1; q2.pop();
-        return max(a1*b1,a2*b2);
+        // int a2=q2.top()-1; q2.pop();
+        // int b2=q2.top()-1; q2.pop();
+        // return max(a1*b1,a2*b2);
+        return a1*b1;
         
         
     }
