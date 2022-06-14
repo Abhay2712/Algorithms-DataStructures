f4e8439668f962eed945a0c5aa5e5a32fb0031e5
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-15 01:02:29 +0530

    Time: 3 ms (90.26%), Space: 7.4 MB (31.59%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
+    bool dfs(string &s, string &p, int i, int j,vector<vector<int>>& dp){
+        
+        if(i>=s.size() and j>=p.size()) return true; //if both s and p get read at the same time. Return true. We have a match.
+        if(j>=p.size()) return false; //if s string hasnt been read fully but p string has been traveresed entirely, return false
+        
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        //declaring a variable to check whether the first characters of s and p are a match or not
+        bool match = i<s.size() and (s[i] == p[j] or p[j] == '.');  //a dot is a match too
+        
+        //checking for second character
+        if((j+1)<p.size() and p[j+1] == '*'){
+            bool dontTake = dfs(s,p,i,j+2,dp); //we do a j+2 because we want to get the next character and not land on the star itself
+            bool take = match ? dfs(s,p,i+1,j,dp) : false; //we take only if the first characters are a match
+            return dp[i][j] = take or dontTake;
+        }
+        
+        if(match) return dp[i][j] = dfs(s,p,i+1,j+1,dp); //simple comparison without any star or dot
+        
+        return dp[i][j] = false;
+    }
     bool isMatch(string s, string p) {
-        /**
-         * f[i][j]: if s[0..i-1] matches p[0..j-1]
-         * if p[j - 1] != '*'
-         *      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
-         * if p[j - 1] == '*', denote p[j - 2] with x
-         *      f[i][j] is true iff any of the following is true
-         *      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
-         *      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
-         * '.' matches any single character
-         */
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
-        
-        f[0][0] = true;
-        for (int i = 1; i <= m; i++)
-            f[i][0] = false;
-        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
-        for (int j = 1; j <= n; j++)
-            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
-        
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= n; j++)
-                if (p[j - 1] != '*')
-                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
-                else
-                    // p[0] cannot be '*' so no need to check "j > 1" here
-                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
-        
-        return f[m][n];
+        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return dfs(s,p,0,0,dp);
     }
 };
