c3b3c88efaf2c92c7058fee567ca2ae889c5a925
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-16 15:19:50 +0530

    Time: 64 ms (82.50%), Space: 8.4 MB (96.01%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -3,15 +3,15 @@ public:
     int dp[102][102] = {};
     int dfs(vector<int>& cuts, int i, int j) {
         if (i>j) return 0;
-        int mini=INT_MAX;
+        
         if (!dp[i][j]) {
             dp[i][j] = INT_MAX;
             for (auto k = i; k <=j; k++)
-                dp[i][j] = min(dp[i][j], 
-                    cuts[j+1] - cuts[i-1] + dfs(cuts, i, k-1) + dfs(cuts, k+1, j));
+                dp[i][j] = min(dp[i][j], cuts[j+1] - cuts[i-1] + dfs(cuts, i, k-1) + dfs(cuts, k+1, j));
         }
         return dp[i][j];
     }
+    
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
