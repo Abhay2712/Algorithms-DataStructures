24b9d5b3db1137822d02384f951bda503e4deef5
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-21 03:15:10 +0530

    Time: 971 ms (8.88%), Space: 51.2 MB (5.73%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return subfun(0,nums,dp);
+    }
+    
+    bool subfun(int i,vector<int>& nums,vector<int>&dp){
+        if(i>=nums.size()-1) return true;
+        
+        if(dp[i]!=-1) return dp[i];
+        int val=nums[i];
+        bool res=0;
+        while(val){
+            if(subfun(i+val,nums,dp)) return dp[i]=1;
+            val--;
+        }
+        return dp[i]=0;   
+    }
+};
