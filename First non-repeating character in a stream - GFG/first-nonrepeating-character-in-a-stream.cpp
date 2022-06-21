9e70f60330c8af17ff0018d2a6569860aec7aeb5
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-21 13:36:26 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -6,25 +6,27 @@ using namespace std;
 class Solution {
 public:
 string FirstNonRepeating(string A){
-    unordered_map<char,int> mp;
-	list<char> l;
-	string ans = "";
-	int curSize = 0,prevSize = 0;
-	for(int i = 0; i<A.length(); i++){
-		mp[A[i]]++;
-		curSize = mp.size();
-		if(curSize>prevSize){
-			l.push_back(A[i]);
+    string ans = "";
+	queue<char> q;
+	int freq[26];
+	memset(freq, 0, sizeof(freq));
+		    
+	freq[A[0]-'a']++;
+	ans+=A[0];
+	q.push(A[0]);
+		    
+	for(int i=1;i<A.size();i++){
+		freq[A[i]-'a']++;
+		if(A[i] == q.front()){
+			while(!q.empty() && freq[q.front()-'a']>1)
+			{
+		       q.pop();
+			}
+		}else{
+		    if(freq[A[i]-'a']==1) q.push(A[i]);
 		}
-		else{
-			auto it = find(l.begin(),l.end(),A[i]);
-    		if(it!=l.end())l.erase(it);
-		}
-		if(!l.empty()){
-			ans.push_back(l.front());
-		}
-		else ans.push_back('#');
-		prevSize = curSize;
+		        
+		ans+= (q.empty() ? '#' : q.front());
 	}
 	return ans;
 }
