5510c1f12e74d78bd0191d4dcf4b48f1f2954e6a
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-22 19:01:00 +0530

    Time: 580 ms (5.01%), Space: 70.6 MB (5.37%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,11 +1,32 @@
 class Solution {
+private:
+    void solve(vector<int>& nums,set<vector<int>> &ans,vector<int> t){
+        if(t.size()==nums.size()){           
+            ans.insert(t);              
+            return ;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=11){            // check if the element is already included
+                int j=nums[i];
+                nums[i]=11;        // make the current ele to 11 so that it is not inserted in next call
+                t.push_back(j);      
+                solve(nums,ans,t);  
+                nums[i]=j;      // backtrack 
+                t.pop_back();
+            }
+        }
+    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end()))
-            ans.push_back(nums);
+        set<vector<int>> s;          //to store unique permutations
+        vector<vector<int>> ans;
+        
+        solve(nums,s,{});
+        
+        for(auto i:s){
+            ans.push_back(i);
+        }
         return ans;
     }
 };
