d677d9bca909d673f951e86f06bc1065f741b3fa
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-22 19:06:20 +0530

    Time: 9 ms (70.49%), Space: 9.2 MB (61.38%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,32 +1,20 @@
 class Solution {
-private:
-    void solve(vector<int>& nums,set<vector<int>> &ans,vector<int> t){
-        if(t.size()==nums.size()){           
-            ans.insert(t);              
-            return ;
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
         }
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=11){            // check if the element is already included
-                int j=nums[i];
-                nums[i]=11;        // make the current ele to 11 so that it is not inserted in next call
-                t.push_back(j);      
-                solve(nums,ans,t);  
-                nums[i]=j;      // backtrack 
-                t.pop_back();
-            }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
         }
     }
-public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> s;          //to store unique permutations
-        vector<vector<int>> ans;
-        
-        solve(nums,s,{});
-        
-        for(auto i:s){
-            ans.push_back(i);
-        }
-        return ans;
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
