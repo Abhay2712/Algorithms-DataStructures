6e87377bd86b2630f696f006e4bd72df1e084a87
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-27 23:56:25 +0530

    Time: 599 ms (21.93%), Space: 117 MB (89.79%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -0,0 +1,28 @@
+class MedianFinder {
+private:
+    priority_queue<int> firstQ; // max_heap for the first half
+    priority_queue<int, vector<int>, greater<int> > secQ; // min_heap for the second half
+public:
+    
+    void addNum(int num) {
+        if(firstQ.empty() || (firstQ.top()>num)) firstQ.push(num); // if it belongs to the smaller half
+        else secQ.push(num); 
+        
+        // rebalance the two halfs to make sure the length difference is no larger than 1
+        if(firstQ.size() > (secQ.size()+1))
+        {
+            secQ.push(firstQ.top());
+            firstQ.pop();
+        }
+        else if(firstQ.size()+1<secQ.size())
+        {
+            firstQ.push(secQ.top());
+            secQ.pop();
+        }
+    }
+
+    double findMedian() {
+        if(firstQ.size() == secQ.size()) return firstQ.empty()?0:( (firstQ.top()+secQ.top())/2.0);
+        else return (firstQ.size() > secQ.size())? firstQ.top():secQ.top(); 
+    }
+};
