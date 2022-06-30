d9919cb1de4206d1823306f8996eaf1bbe289972
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-06-30 15:21:46 +0530

    Time: 55 ms (66.84%), Space: 16.8 MB (35.95%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -4,12 +4,8 @@ public:
         int result = 0;
         for(int i=0; i<points.size();++i){
             unordered_map<long double,int> h;
-            int same = 1;
             int localmax = 0;
             for(int j=i+1;j<points.size();++j){
-                // if(points[i][0] == points[j][0] && points[i][1]==points[j][1]){
-                //     same++;
-                // }
                 if(points[i][0]==points[j][0]){
                     h[INT_MAX]++;
                 }
@@ -21,7 +17,7 @@ public:
             for(auto i : h){
                 localmax = max(localmax,i.second);
             }
-            localmax +=same;
+            localmax +=1;
             result = max(result,localmax);
         }
         return result;
