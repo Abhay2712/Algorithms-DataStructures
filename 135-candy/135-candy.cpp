d981d638021ca55bed059933a5c29aae062fd911
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-05 00:14:55 +0530

    Time: 34 ms (49.44%), Space: 17.1 MB (96.69%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,3 +1,6 @@
+//hard question
+//We can consider this problem like valley and peak problem. In each valley there should be 1 candy and for each increasing solpe in either side we need to increse candy by 1. Peaks have highest candy. If any equal rating is found then candy resets to 1 as two equal neighbours may have any number of candies. The peak should contain the higher number of candy between which is calculated from the incresing slope and which is calculated from decreasing slope. Because this will satisfy the condition that peak element is having more candies than its neighbours.
+
 class Solution {
 public:
     int candy(vector<int>& ratings) {
