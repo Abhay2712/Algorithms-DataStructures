a2f1b57f2a448f1d7f5e4f373eee41ef07401593
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-08 19:38:24 +0530

    Time: 81 ms (84.04%), Space: 10.7 MB (92.29%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int dp[100][100][21];   
+    const int mx = 1e7+1;
+    
+    int f(vector<int>& houses, vector<vector<int>>& cost, int target, int idx, int count, int prev){
+        //base cases
+        if(idx == houses.size()) return count==target ? 0 : mx;
+        if(idx > houses.size()) return mx;
+         
+        if(dp[idx][count][prev]!=-1) return dp[idx][count][prev];
+        
+        int minCost = mx;
+        //If already painted
+        if(houses[idx]){
+            
+            //If previous is different, then it will increase count by 1
+            if(houses[idx]!=prev) minCost = f(houses,cost,target,idx+1,count+1,houses[idx]);
+            
+            else minCost = f(houses,cost,target,idx+1,count,houses[idx]);
+            
+        }else{
+            //we have to try all the colors
+            for(int j=0;j<cost[0].size();j++){
+                int tmp;  //store the current cost
+                
+                //f previous is different, then it will increase count by 1
+                if((j+1)!=prev) tmp = cost[idx][j] + f(houses,cost,target,idx+1,count+1,j+1);
+                
+                else tmp = cost[idx][j] + f(houses,cost,target,idx+1,count,j+1);
+                
+                minCost = min(minCost,tmp);
+            }
+        }
+        //We have to store before returning
+        return dp[idx][count][prev] = minCost;
+    }
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        memset(dp,-1,sizeof(dp));
+        
+        // 0 0 0 represents the current index(idx) , neighbourhood count(count), color of prev house(prev)
+        int ans = f(houses,cost,target,0,0,0);
+        
+        
+        if(ans==mx) return -1;
+        else return ans;
+    }
+};
