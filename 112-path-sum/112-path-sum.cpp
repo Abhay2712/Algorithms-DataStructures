168c942fbf9e8267deacd9365e7fc0bb823e156a
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-11 23:14:25 +0530

    Time: 22 ms (28.21%), Space: 21.3 MB (39.65%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -9,28 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int target) {
-        queue<pair<TreeNode*,int>>q;
-        if(!root) return false;
-        q.push(make_pair(root,target-root->val));
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                auto start=q.front();
-                TreeNode *temp=start.first;
-                int val=start.second;
-                q.pop();
-                if(!temp->left && !temp->right && val==0) return true;
-                
-                if(temp->left) q.push(make_pair(temp->left,val-temp->left->val));
-                if(temp->right) q.push(make_pair(temp->right,val-temp->right->val));
-                
-            }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL)
+        {
+            return false;
         }
-        return false;
-        
-        
+        targetSum -= root->val;
+        if(targetSum==0 && root->left == NULL && root->right == NULL)
+        {
+            return true;
+        }
+        return(hasPathSum(root->left, targetSum)|| hasPathSum(root->right, targetSum));
     }
 };
+
+
+//BFS solution
+// class Solution {
+// public:
+//     bool hasPathSum(TreeNode* root, int target) {
+//         queue<pair<TreeNode*,int>>q;
+//         if(!root) return false;
+//         q.push(make_pair(root,target-root->val));
+//         while(!q.empty()){
+//             int n=q.size();
+//             while(n--){
+//                 auto start=q.front();
+//                 TreeNode *temp=start.first;
+//                 int val=start.second;
+//                 q.pop();
+//                 if(!temp->left && !temp->right && val==0) return true;
+                
+//                 if(temp->left) q.push(make_pair(temp->left,val-temp->left->val));
+//                 if(temp->right) q.push(make_pair(temp->right,val-temp->right->val));
+                
+//             }
+//         }
+//         return false;
+        
+        
+//     }
+// };
