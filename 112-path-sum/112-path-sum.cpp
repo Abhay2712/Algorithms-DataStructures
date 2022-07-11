1b028a8900607336c25f0e99d4ac5ecff6b318e0
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-11 23:09:33 +0530

    Time: 23 ms (24.41%), Space: 21.4 MB (39.65%) - LeetHub

---
+++ 112-path-sum/112-path-sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int target) {
+        queue<pair<TreeNode*,int>>q;
+        if(!root) return false;
+        q.push(make_pair(root,target-root->val));
+        while(!q.empty()){
+            int n=q.size();
+            while(n--){
+                auto start=q.front();
+                TreeNode *temp=start.first;
+                int val=start.second;
+                q.pop();
+                if(!temp->left && !temp->right && val==0) return true;
+                
+                if(temp->left) q.push(make_pair(temp->left,val-temp->left->val));
+                if(temp->right) q.push(make_pair(temp->right,val-temp->right->val));
+                
+            }
+        }
+        return false;
+        
+        
+    }
+};
