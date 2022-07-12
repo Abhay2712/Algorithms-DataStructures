08f3cf72414749cde39b7cf530c188bfe0325b83
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-12 13:53:36 +0530

    Time: 794 ms (24.64%), Space: 10.2 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool solve(int i,vector<int> &arr,vector<bool> &vis,int sum,int rq,int t){
+    bool solve(int i,vector<int> &arr,vector<bool> &vis,int sum,int req,int t){
         if(t == 1) return true;
-        if(sum == rq) return solve(0,arr,vis,0,rq,t-1);
+        if(sum == req) return solve(0,arr,vis,0,req,t-1); //if we found a sum equal to required, again start from beginning to find another group with the required sum
         if(i == arr.size()) return false;
         
-        if(!vis[i] and sum + arr[i] <= rq){
+        if(!vis[i] && sum + arr[i] <= req){
             vis[i] = true;
-            if(solve(i+1,arr,vis,sum+arr[i],rq,t)) return true;
+            if(solve(i+1,arr,vis,sum+arr[i],req,t)) return true; //pick
             vis[i] = false;                    
         }
         
-        return solve(i+1,arr,vis,sum,rq,t);
+        return solve(i+1,arr,vis,sum,req,t); //not pick
     }
-    bool makesquare(vector<int>& arr) {
+    
+    bool makesquare(vector<int>&arr) {
         int sum = accumulate(arr.begin(),arr.end(),0);
-        if(arr.size() < 4 or sum % 4) return false;
+        if(arr.size() < 4 || sum % 4) return false;   
         vector<bool> vis(arr.size());
         return solve(0,arr,vis,0,sum/4,4);
     }
