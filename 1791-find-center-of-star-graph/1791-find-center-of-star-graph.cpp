3a87c710bc34dc7db3004a5747322946968d976e
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-15 22:50:14 +0530

    Time: 390 ms (25.36%), Space: 67.2 MB (82.66%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int a=edges[0][0];
-        int b=edges[0][1];
-        int c=edges[1][0];
-        int d=edges[1][1];
-        if(a==b || a==c || a==d) return a;
-        else if(b==c || b==d) return b;
-        else return c;
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        else
+            return edges[0][1];
     }
 };
