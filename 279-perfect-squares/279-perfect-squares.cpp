969d87040b1acf1d2ec4f5e57d200ceb878c053b
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-20 21:33:36 +0530

    Time: 332 ms (41.06%), Space: 9.6 MB (36.23%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subfun(int n,vector<int> &dp){
+        if(n==0){
+           dp[0]=0;
+           return 0;
+       }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        int minvalue=INT_MAX;
+        for(int i=1;i*i<=n;i++){
+            int rem=subfun(n-i*i,dp);
+            minvalue=min(minvalue,rem+1);
+        }
+        // minvalue+=1;
+        return dp[n]=minvalue;
+    // return minvalue;  
+    }
+    
+    int numSquares(int n) {
+       vector<int>dp(n+1,-1);
+       return subfun(n,dp);
+    }
+};
