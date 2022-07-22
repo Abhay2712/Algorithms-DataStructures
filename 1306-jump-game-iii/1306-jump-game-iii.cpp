b60f1b6b43194af6fdfd45ae210f12dc481afb4f
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-22 19:02:12 +0530

    Time: 89 ms (28.55%), Space: 30.6 MB (99.61%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
-        if(start>=0&&start<n&&arr[start]>=0)
+        if(start>=0 && start<n && arr[start]>=0)
         {
             if(arr[start]==0)
                 return true;
