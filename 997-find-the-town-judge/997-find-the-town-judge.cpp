4ab898a9499fd3e44a01788011098677e4609110
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-23 12:26:46 +0530

    Time: 268 ms (52.39%), Space: 60.9 MB (67.54%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>>recieve(n+1,vector<int>());
-        vector<vector<int>>send(n+1,vector<int>());
-        for(auto i:trust){
-            recieve[i[1]].push_back(i[0]);
-            send[i[0]].push_back(i[1]);
+        vector<int> trusts (n+1, 0) ;
+        vector<int> trustedby (n+1, 0);
+
+        for(int i = 0 ; i< trust.size() ; i++)
+        {
+            trusts[trust[i][0]]++ ;
+            trustedby[trust[i][1]] ++ ;
         }
         
-        for(int i=1;i<=n;i++){
-            if(recieve[i].size()==n-1 && send[i].size()==0) return i;
-        }
-        return -1;   
+        for(int i = 1 ; i<=n ; i++)   
+            if(trusts[i] == 0 && trustedby[i] == n-1) return i ;
+        
+        return -1 ; 
     }
 };
