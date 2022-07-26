de9d76af21de59922965612013eeeb4b30d1f9b1
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-26 10:28:47 +0530

    Time: 38 ms (63.35%), Space: 8.4 MB (44.21%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int maxi=1e9+7;
-    int subfun(int i,int k,int target,vector<vector<vector<int>>>&dp){
+    int subfun(int i,int k,int target,vector<vector<int>>&dp){
         if(i==0 && target==0) return 1;
         if(i<0 || target<0) return 0;
         
-        if(dp[i][k][target]!=-1) return dp[i][k][target];
+        if(dp[i][target]!=-1) return dp[i][target];
         int sum=0;
         for(int j=1;j<=k;j++){
             sum=(sum+subfun(i-1,k,target-j,dp))%maxi;
         }
-        return dp[i][k][target]=sum%maxi;
+        return dp[i][target]=sum%maxi;
     }
     
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(k+1,vector<int>(target+1,-1)));
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
         return subfun(n,k,target,dp);
     }
 };
