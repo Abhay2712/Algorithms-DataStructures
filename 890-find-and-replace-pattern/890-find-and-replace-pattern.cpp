81057443552dc28021b60c52eebab8c01f0a87f3
Abhay Sharma <68679022+Abhay2712@users.noreply.github.com>
2022-07-29 16:51:29 +0530

    Time: 3 ms (90.27%), Space: 8.5 MB (43.97%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string> words, string p) {
+        vector<string> res;
+        p = F(p);
+        for (string w : words) if (F(w) == p) res.push_back(w);
+        return res;
+    }
+    
+    string F(string w) {
+        unordered_map<char, int> m;
+        for (char c : w) if (!m.count(c)) m[c] = m.size();
+        for (int i = 0; i < w.length(); ++i) w[i] = 'a' + m[w[i]];
+        return w;
+    }
+};
